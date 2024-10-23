a238c71546b946321aea86a4ad49c1f8c44cd148
ErikBoonprakong <e.boonprakong@gmail.com>
2024-10-23 15:25:00 +0100

    added read more / show less button to bio

--- my-portfolio/src/Components/Entry.js
+++ my-portfolio/src/Components/Entry.js
@@ -10,6 +10,13 @@ import Pdf from './Erik Boonprakong-Kitching - CV.pdf'
 class Entry extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        showMore: false,
+    }
+  }
+
+  handleShowMore = () => {
+    this.setState({showMore : !this.state.showMore})
   }
 
   render() {
@@ -35,7 +42,12 @@ class Entry extends React.Component {
             with commercial experience in full-stack development utilising C#,
             .NET, JavaScript, React, Node.js.
           </p>
-          <p className="intro-header">
+
+          <p
+            className={
+              this.state.showMore === false ? "hide-text" : "intro-header"
+            }
+          >
             I began my career in software engineering as a Full-Stack Engineer
             at a startup called Sigma Labs. During my time here I worked as the
             software architect in a 3-person team to create an online
@@ -45,20 +57,31 @@ class Entry extends React.Component {
             particularly enjoyed the challenge of having to learn many new
             technologies on the fly while still adhering to tight deadlines.
           </p>
-          <p className="intro-header">
+          <p
+            className={
+              this.state.showMore === false ? "hide-text" : "intro-header"
+            }
+          >
             I recently worked as a contract C# Developer at Lloyds Banking Group
             where I directly contributed to improving the bank's security by
             integrating a newly-created, harder to manipulate data type into
             their software that provided information about bond prices. My
             initial 6-month contract was then extended in light of this work.
           </p>
-          <p className="intro-header">
+          <p
+            className={
+              this.state.showMore === false ? "hide-text" : "intro-header"
+            }
+          >
             Since Lloyds I've continued to upskill by doing freelance web
             development for an art school that I used to work at. I chose to
             take on this work because I knew the school needed a website and I
             wanted to continue practicing working to a list of user requirements
             provided by someone else. In my free time I've been working on a
             continually deployed, full-stack ASP.NET Core web app.
+          </p>
+          <p className="intro-header read-more" onMouseDown={this.handleShowMore}>
+            {this.state.showMore === false ? 'Read more...' : 'Show less.'}
           </p>
           <p></p>
           <hr />
